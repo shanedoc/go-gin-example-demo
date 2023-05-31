3106c8bcfd9b11901118ac55f2fa750a019fc68b
shanedoc <shanedoc@163.com>
2023-05-31 17:45:19 +0800

    文章模块

---
+++ routers/api/v1/article.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+//文章
+
+//详情
+func GetArticle(c *gin.Context) {
+
+}
+
+//列表
+func GetArticles(c *gin.Context) {
+
+}
+
+//新增
+func AddArticle(c *gin.Context) {
+
+}
+
+//编辑
+func EditArticle(c *gin.Context) {
+}
+
+//删除文章
+func DeleteArticle(c *gin.Context) {
+}
