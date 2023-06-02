cd8ffb2e09f132e23bee9a69fbd7a2f2d39160c0
shanedoc <shanedoc@163.com>
2023-06-02 17:46:48 +0800

    model callback

--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"go-gin-example/pkg/setting"
 	"go-gin-example/routers"
-	"net/http"
+	"log"
+	_ "net/http"
+	"syscall"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
-	s := &http.Server{
-		Addr:           fmt.Sprintf("%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
-	s.ListenAndServe()
-	router.Run(":8009")
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
